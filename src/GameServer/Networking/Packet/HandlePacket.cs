bb3ca1dbe2c120669336a547ef0b1e95d287d6d6
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-07-09 13:24:18 -0500

    Clean Slate

--- src/GameServer/Networking/Packet/HandlePacket.cs
+++
@@ -1,7 +0,0 @@
-namespace GameServer.Networking.Packet
-{
-    public abstract class HandlePacket
-    {
-        public virtual void Run(params object[] args) {}
-    }
-}
---
+++ src/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using ENet;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			ENet.Library.Initialize();
+
+			int maxClients = 100;
+			ushort port = 8888;
+			using (var server = new Host())
+			{
+                var address = new Address
+                {
+                    Port = port
+                };
+
+                server.Create(address, maxClients);
+
+                while (!Console.KeyAvailable)
+                {
+                    var polled = false;
+
+                    while (!polled)
+                    {
+                        if (server.CheckEvents(out Event netEvent) <= 0)
+                        {
+                            if (server.Service(15, out netEvent) <= 0)
+                                break;
+
+                            polled = true;
+                        }
+
+                        switch (netEvent.Type)
+                        {
+                            case EventType.None:
+                                break;
+
+                            case EventType.Connect:
+                                Console.WriteLine("Client connected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Disconnect:
+                                Console.WriteLine("Client disconnected - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Timeout:
+                                Console.WriteLine("Client timeout - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP);
+                                break;
+
+                            case EventType.Receive:
+                                Console.WriteLine("Packet received from - ID: " + netEvent.Peer.ID + ", IP: " + netEvent.Peer.IP + ", Channel ID: " + netEvent.ChannelID + ", Data length: " + netEvent.Packet.Length);
+                                netEvent.Packet.Dispose();
+                                break;
+                        }
+                    }
+                }
+
+                server.Flush();
+			}
+
+			ENet.Library.Deinitialize();
+		}
+    }
+}
