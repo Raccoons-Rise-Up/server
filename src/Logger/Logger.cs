fac538ea15cdbeb22494d32999b389e696e51bb0
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-07-28 19:13:31 -0500

    Working Console

--- src/Logger/Logger.cs
+++ src/Logger/Logger.cs
@@ -15,61 +15,60 @@ namespace GameServer
         public static void WorkerThread()
         {
             var loggerMessageRow = 0;
-            //var textFieldRow = Console.WindowHeight - 2;
-            var textFieldRow = 10;
+            var textFieldRow = 1;
+            var textFieldRowExpansion = 0;
 
-            var messageLines = 0;
+            var input = "";
 
             //Program.StartServer(); // Finished setting up Logger, now start the server
 
-            m_Messages.Enqueue("Hello world");
-            //new Thread(Test).Start();
+            //m_Messages.Enqueue("Hello world");
 
             while (true)
             {
-                while (m_Messages.TryDequeue(out string message))
+                /*while (m_Messages.TryDequeue(out string message))
                 {
                     //Console.WriteLine(message);
+                }*/
+
+                Console.CursorTop = textFieldRow + textFieldRowExpansion;
+                var keyInfo = Console.ReadKey(true);
+                Console.Write(keyInfo.KeyChar);
+                if (Console.CursorLeft == Console.WindowWidth - 1) 
+                {
+                    textFieldRowExpansion++;
+                    Console.CursorLeft = 0;
                 }
+                    
 
-                
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    // Clear the text input field
+                    for (int i = 0; i < textFieldRowExpansion + 1; i++) 
+                    {
+                        Console.Write(new string(' ', Console.WindowWidth));
+                        Console.CursorTop -= 2;
+                        Console.CursorLeft = 0;
+                    }
 
-                // Set cursor to the beginning of the text input field
-                Console.SetCursorPosition(0, textFieldRow);
 
-                // Clear the text input field
-                Console.Write(new string(' ', Console.WindowWidth));
+                    Console.CursorTop = loggerMessageRow;
+                    Console.WriteLine(input);
 
-                // If the logger message row is >= text field row then move the text field row one down
-                if (loggerMessageRow >= textFieldRow)
-                    textFieldRow += messageLines;
+                    var lines = (int)Math.Ceiling((float)input.Length / Console.WindowWidth);
+                    input = "";
 
-                // Set cursor back to the beginning of the text input field
-                Console.SetCursorPosition(0, textFieldRow);
+                    
 
-                // Read the command the user typed and pressed enter
-                var input = Console.ReadLine();
-
-                var lines = (int)Math.Ceiling(input.Length / (float)Console.WindowWidth);
-
-                // Set cursor to logging area
-                Console.SetCursorPosition(0, loggerMessageRow);
-
-                // Print the message to the logger (Console.Write() is not used as everything would get distorted the window gets resized)
-                Console.WriteLine(input);
-
-                // Increment row by the number of lines from input
-                loggerMessageRow += lines;
-                messageLines = lines;
-            }
-        }
-
-        private static void Test() 
-        {
-            while (true) 
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Yes");
+                    loggerMessageRow += lines;
+                    textFieldRow += lines;
+                    textFieldRowExpansion = 0;
+                }
+                else 
+                {
+                    var ch = keyInfo.KeyChar;
+                    input += ch;
+                }
             }
         }
     }
