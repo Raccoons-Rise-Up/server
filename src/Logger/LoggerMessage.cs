15b07bbc174a4e41018abc9bf8d1e179ddd95022
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-07-31 21:14:40 -0500

    Create LoggerMessage.cs

---
+++ src/Logger/LoggerMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameServer
+{
+    public class LoggerMessage
+    {
+        public readonly string m_Message;
+
+        public LoggerMessage(string message) 
+        {
+            m_Message = message;
+        }
+
+        public int GetLines() 
+        {
+            var lines = 0;
+            lines += (int)Math.Ceiling((double)m_Message.Length / Console.WindowWidth);
+            lines += m_Message.Split('\n').Length - 1;
+            return lines;
+        }
+    }
+}
