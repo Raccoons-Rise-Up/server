f59b3e4683082ad4170d341a20be7da6f23657c9
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-23 20:47:29 -0500

    CommandClear

---
+++ src/Console/Commands/CommandClear.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameServer.Database;
+using GameServer.Server;
+
+namespace GameServer.Logging.Commands
+{
+    public class CommandClear : Command
+    {
+        public override void Run(string[] args) 
+        {
+            if (args.Length == 0)
+            {
+                Logger.Log("Please provide a username to search for");
+                return;
+            }
+
+            using var db = new DatabaseContext();
+
+            var dbPlayer = db.Players.ToList().Find(x => x.Username == args[0]);
+
+            if (dbPlayer == null)
+            {
+                Logger.Log($"The player with the username '{args[0]}' can not be found in the database");
+                return;
+            }
+
+            // Remove the player from the database
+            db.Remove(dbPlayer);
+            db.SaveChanges();
+
+            // Clear the players variables from players list
+            var instructionClearPlayerStats = new ServerInstruction(ServerInstructionOpcode.ClearPlayerStats);
+            instructionClearPlayerStats.Write(dbPlayer.Username);
+
+            ENetServer.serverInstructions.Enqueue(instructionClearPlayerStats);
+
+            Logger.Log($"Cleared {dbPlayer.Username} from database");
+        }
+    }
+}
