baef98d6af6698263c8a63cda43a8e4ef169bc9d
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-25 20:56:49 -0500

    Command Ban

---
+++ src/Console/Commands/CommandBan.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using GameServer.Database;
+using GameServer.Server;
+
+namespace GameServer.Logging.Commands
+{
+    public class CommandBan : ICommand
+    {
+        public string Description { get; set; }
+        public string Usage { get; set; }
+        public string[] Aliases { get; set; }
+
+        public CommandBan() 
+        {
+            Description = "Ban a specified player";
+            Usage = "<player>";
+        }
+
+        public void Run(string[] args) 
+        {
+            if (args.Length == 0)
+            {
+                Logger.Log("Please provide a player name to ban");
+                return;
+            }
+
+            var cmd = new ServerInstructions();
+            cmd.Set(ServerInstructionOpcode.BanPlayer, args[0]);
+
+            ENetServer.ServerInstructions.Enqueue(cmd);
+        }
+    }
+}
