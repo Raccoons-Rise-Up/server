6bc7cf3bd806283faaf28747fac052cb10573acc
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-25 20:57:00 -0500

    Command Clear

--- src/Console/Commands/CommandClear.cs
+++ src/Console/Commands/CommandClear.cs
@@ -1,47 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GameServer.Database;
-using GameServer.Server;
+using GameServer.Logging;
 
 namespace GameServer.Logging.Commands
 {
-    public class CommandClear : Command
+    public class CommandClear : ICommand
     {
-        public override void Run(string[] args) 
+        public string Description { get; set; }
+        public string Usage { get; set; }
+        public string[] Aliases { get; set; }
+
+        public CommandClear()
         {
-            using var db = new DatabaseContext();
+            Description = "Clear the console";
+            Aliases = new string[] { "cls" };
+        }
 
-            if (args.Length == 0)
-            {
-                
-                db.RemoveRange(db.Players);
-                db.SaveChanges();
-                Logger.Log($"Cleared database. There are {db.Players.Count()} players left in database");
-                return;
-            }
-
-            var dbPlayer = db.Players.ToList().Find(x => x.Username == args[0]);
-
-            if (dbPlayer == null)
-            {
-                Logger.Log($"The player with the username '{args[0]}' can not be found in the database");
-                return;
-            }
-
-            // Remove the player from the database
-            db.Remove(dbPlayer);
-            db.SaveChanges();
-
-            // Clear the players variables from players list
-            var cmd = new ServerInstructions();
-            cmd.Set(ServerInstructionOpcode.ClearPlayerStats, dbPlayer.Username);
-
-            ENetServer.ServerInstructions.Enqueue(cmd);
-
-            Logger.Log($"Cleared {dbPlayer.Username} from database");
+        public void Run(string[] args)
+        {
+            Logger.TextField.row = 1;
+            Logger.TextField.column = 0;
+            Logger.LoggerMessageRow = 0;
+            Console.CursorLeft = 0;
+            Console.CursorTop = 0;
+            Console.Clear();
         }
     }
 }
