1b5fa0692aac2338af09b7f7ce1b24716374b24d
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-12-23 14:50:10 -0600

    PacketPlayerJoined

---
+++ src/Server/Packets/Write/WPacketPlayerJoined.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Common.Networking.IO;
+using Common.Networking.Message;
+using Common.Networking.Packet;
+using Common.Game;
+
+namespace GameServer.Server.Packets
+{
+    public class WPacketPlayerJoined : IWritable
+    {
+        public uint PlayerId { get; set; }
+        public string PlayerName { get; set; }
+
+        public void Write(PacketWriter writer)
+        {
+            writer.Write(PlayerId);
+            writer.Write(PlayerName);
+        }
+    }
+}
