bce64bf3eb70514d3ebe5a30bc636c34a443d8ac
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-12-23 14:50:00 -0600

    PacketPlayerList

---
+++ src/Server/Packets/Write/WPacketPlayerList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Common.Networking.IO;
+using Common.Networking.Message;
+using Common.Networking.Packet;
+using Common.Game;
+
+namespace GameServer.Server.Packets
+{
+    public class WPacketPlayerList : IWritable
+    {
+        public void Write(PacketWriter writer)
+        {
+            writer.Write((byte)ENetServer.Players.Count);
+            foreach (var p in ENetServer.Players) 
+            {
+                writer.Write(p.Key);
+                writer.Write(p.Value.Username);
+            }
+        }
+    }
+}
