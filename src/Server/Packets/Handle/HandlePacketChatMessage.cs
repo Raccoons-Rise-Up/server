c83827511ac4f2605e4db1990291e50f9f63581e
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-12-23 18:53:46 -0600

    Better Chat Packet Handling

--- src/Server/Packets/Handle/HandlePacketChatMessage.cs
+++ src/Server/Packets/Handle/HandlePacketChatMessage.cs
@@ -15,6 +15,15 @@ namespace GameServer.Server.Packets
 
         public HandlePacketChatMessage() => Opcode = ClientPacketOpcode.ChatMessage;
 
+        private readonly string[] BlockedStrings = new string[] 
+        {
+            "fuck",
+            "nigga",
+            "nigger",
+            "cunt",
+            "bitch"
+        };
+
         public override void Handle(Event netEvent, ref PacketReader packetReader)
         {
             var data = new RPacketChatMessage();
@@ -24,13 +33,23 @@ namespace GameServer.Server.Packets
 
             var player = ENetServer.Players[peer.ID];
 
+            var message = data.Message;
+
+            // Remove any trailing spaces at start and end
+            message = message.Trim();
+
+            // Swear filter
+            foreach (var blockedString in BlockedStrings)
+                message = message.Replace(blockedString, "meowww");
+
             // Not sure if this is allowed with threading but going to try anyways!
             ENetServer.Send(new ServerPacket((byte)ServerPacketOpcode.ChatMessage, new WPacketChatMessage
             {
-                Message = data.Message
-            }), ENetServer.GetOtherPeers(peer));
+                PlayerId = peer.ID,
+                Message = message
+            }), ENetServer.GetAllPeers());
 
-            Logger.Log($"{player.Username}: {data.Message}");
+            Logger.Log($"{player.Username}: {message}");
         }
     }
 }
